1c383067637732eb55f3e13b5f2da6bc03a1b337
Yasuu19 <yasminegherbibahia@gmail.com>
2021-11-22 12:33:47 +0100

    [FIX] adaptation des missions au Dieu des cavernes

--- src/fr/pepintrie/pepintrieplugin/gods/CaveGod.java
+++ src/fr/pepintrie/pepintrieplugin/gods/CaveGod.java
@@ -23,25 +23,25 @@ public class CaveGod extends God {
 		//goal for size 1
 		goals.get(0).add(new Goal("Aménager un petit coin dans une grotte"));
 		//goal for size 2
-		goals.get(1).add(new Goal("construisant un petit temple"));
-		goals.get(1).add(new Goal("arborant mes temples de ma bannière"));
+		goals.get(1).add(new Goal("construisant un temple modeste"));
+		goals.get(1).add(new Goal("Déployant ma bannière"));
 		//goal for size 3
-		goals.get(2).add(new Goal("construisant un cimeti�re dans la montagne"));
+		goals.get(2).add(new Goal("construisant un cimetière dans les souterrains"));
 		//goal for size 4
-		goals.get(3).add(new Goal("construisant une porte dans la montagne"));
+		goals.get(3).add(new Goal("construisant une porte dans les souterrains"));
 		goals.get(3).add(new Goal("capturant des prisoniers"));
 		//goal for size 5
 		goals.get(4).add(new Goal("construisant une statue en mon honneur"));
 		//goal for size 6
 		goals.get(5).add(new Goal("construisant une crypte"));
-		goals.get(5).add(new Goal("mettant en exposition les richesses de la montagne"));
+		goals.get(5).add(new Goal("mettant en exposition les richesses des entrailles de la terre"));
 		//goal for size 7
 		goals.get(6).add(new Goal("construisant une administration religieuse"));
 		goals.get(6).add(new Goal("construisant un lieu de sacrifice"));
 		//goal for size 8
 		goals.get(7).add(new Goal("construisant une basilique"));
 		//goal for size 9
-		goals.get(8).add(new Goal("construisant une cath�drale"));
+		goals.get(8).add(new Goal("construisant une cathédrale"));
 		//goal for size 10
 		goals.get(9).add(new Goal("construisant une ville sainte"));
 		
