85dd5dc04098db18b26f32474547ff0fd8c2a911
Yasuu19 <yasminegherbibahia@gmail.com>
2021-11-22 12:38:13 +0100

    [FIX] correction des accents

--- src/fr/pepintrie/pepintrieplugin/gods/CliffGod.java
+++ src/fr/pepintrie/pepintrieplugin/gods/CliffGod.java
@@ -12,7 +12,7 @@ public class CliffGod extends God {
 
 	public CliffGod(String name) {
 		super(name);
-		color = "§7";
+		color = "&7";
 		type = GodsType.CLIFF;	
 	}
 
@@ -21,12 +21,12 @@ public class CliffGod extends God {
 	protected void getGoals() {
 		set10Goals(goals);
 		//goal for size 1
-		goals.get(0).add(new Goal("Aménager un petit coin dans une montagne"));
+		goals.get(0).add(new Goal("AmÃ©nager un petit coin dans une montagne"));
 		//goal for size 2
 		goals.get(1).add(new Goal("construisant un petit temple"));
-		goals.get(1).add(new Goal("arborant mes temples de ma banière"));
+		goals.get(1).add(new Goal("arborant mes temples de ma banniÃ¨re"));
 		//goal for size 3
-		goals.get(2).add(new Goal("construisant un cimetière dans la montagne"));
+		goals.get(2).add(new Goal("construisant un cimetiÃ¨re dans la montagne"));
 		//goal for size 4
 		goals.get(3).add(new Goal("construisant une porte dans la montagne"));
 		goals.get(3).add(new Goal("capturant des prisoniers"));
@@ -41,7 +41,7 @@ public class CliffGod extends God {
 		//goal for size 8
 		goals.get(7).add(new Goal("construisant une basilique"));
 		//goal for size 9
-		goals.get(8).add(new Goal("construisant une cathédrale"));
+		goals.get(8).add(new Goal("construisant une cathÃ©drale"));
 		//goal for size 10
 		goals.get(9).add(new Goal("construisant une ville sainte"));
 		
